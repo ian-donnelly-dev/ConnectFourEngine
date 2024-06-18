c3baafe9163c8da4f1989aa7a6a12a32dda45565
Ian Donnelly <hello@ian-donnelly.dev>
2024-06-17 18:03:56 -0700

    chore: add constants for future development

---
+++ Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace ConnectFourEngine
+{
+    public static class Constants
+    {
+        public const int Columns = 7;
+        public const int Rows = 6;
+        
+        public const int MaxValue = 19;
+        public const int MaxDepth = 10;
+    }
+}
