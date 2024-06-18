12e01aa30c76ee954853b0f52f36b306bb8e94da
Ian Donnelly <hello@ian-donnelly.dev>
2024-06-18 12:38:08 -0700

    refactor: reorganize bitboard debug stringify functions

--- Program.cs
+++ Program.cs
@@ -5,56 +5,48 @@ namespace ConnectFourEngine
 {
     static class Program
     {
-        private static string BitboardToString(ulong bitboard, bool multiline)
+        private static string BitboardToString(ulong bitboard)
         {
             StringBuilder sb = new StringBuilder();
-        
-            if (multiline)
+            
+            for (int row = Constants.ROWS - 1; row >= 0; row--)
             {
-                for (int row = Constants.ROWS - 1; row >= 0; row--)
+                for (int col = 0; col < Constants.COLUMNS; col++)
                 {
-                    for (int col = 0; col < Constants.COLUMNS; col++)
+                    int bitIndex = col * Constants.ROWS + row;
+                    sb.Append((bitboard & (1UL << bitIndex)) != 0 ? '1' : '0');
+                    
+                    if (col < Constants.COLUMNS - 1)
                     {
-                        int bitIndex = col * Constants.ROWS + row;
-                        sb.Append((bitboard & (1UL << bitIndex)) != 0 ? '1' : '0');
-                        
-                        if (col < Constants.COLUMNS - 1)
-                        {
-                            sb.Append(' ');
-                        }
-                    }
-                    if (row > 0)
-                    {
-                        sb.AppendLine();
+                        sb.Append(' ');
                     }
                 }
+                if (row > 0)
+                {
+                    sb.AppendLine();
+                }
             }
-            else
-            {
-                string bitString = Convert.ToString((long)bitboard, 2).PadLeft(Constants.ROWS * Constants.COLUMNS, '0');
-                sb.Append(bitString);
-            }
-        
+            
             return sb.ToString();
         }
         
-        private static string UlongToString(ulong value)
+        private static string UlongToString(ulong bitboard, bool cropToBoard = true)
         {
-            return Convert.ToString((long)value, 2).PadLeft(64, '0');
+            int length = cropToBoard ? Constants.ROWS * Constants.COLUMNS : 64;
+            return Convert.ToString((long)bitboard, 2).PadLeft(length, '0');
         }
         
         static void Main()
         {
-            const ulong bitboard = 0b010000000000000000000000000000000000001110;
+            const ulong bitboard = 0b000000_000000_000000_000000_000000_000000_000001;
             
-            Console.WriteLine(BitboardToString(bitboard, true));
+            Console.WriteLine(BitboardToString(bitboard));
             Console.WriteLine();
             
-            Console.WriteLine(BitboardToString(bitboard, false));
+            Console.WriteLine(UlongToString(bitboard));
             Console.WriteLine();
             
-            Console.WriteLine("First Column Mask:");
-            Console.WriteLine(UlongToString((1UL << Constants.ROWS) - 1));
+            Console.WriteLine(UlongToString(bitboard, false));
         }
     }
 }
