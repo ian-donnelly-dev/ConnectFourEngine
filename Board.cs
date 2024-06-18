6200fcca1164dbcc9a18b68c4ac067f89778135b
Ian Donnelly <hello@ian-donnelly.dev>
2024-06-18 16:45:56 -0700

    feat: add bitboard move and unmove functions

--- Board.cs
+++ Board.cs
@@ -7,6 +7,12 @@ namespace ConnectFourEngine
         public ulong Player1Bitboard { get; private set; }
         public ulong Player2Bitboard{ get; private set; }
         
+        public Board()
+        {
+            Player1Bitboard = 0UL;
+            Player2Bitboard = 0UL;
+        }
+        
         public bool IsPlayer1Turn()
         {
             return BitOperations.PopCount(Player1Bitboard) == BitOperations.PopCount(Player2Bitboard);
@@ -17,7 +23,40 @@ namespace ConnectFourEngine
             return isPlayer1 ? BitOperations.PopCount(Player1Bitboard) : BitOperations.PopCount(Player2Bitboard);
         }
         
-        public int GetColumnHeight(int column)
+        public bool IsColumnPlayable(int column)
+        {
+            return GetColumnHeight(column) < Constants.ROWS;
+        }
+        
+        public void MakeMove(int column)
+        {
+            ulong moveBit = 1UL << GetColumnHeight(column) + column * Constants.ROWS;
+
+            if (IsPlayer1Turn())
+            {
+                Player1Bitboard |= moveBit;
+            }
+            else
+            {
+                Player2Bitboard |= moveBit;
+            }
+        }
+
+        public void UnmakeMove(int column)
+        {
+            ulong moveBit = 1UL << GetColumnHeight(column) - 1 + column * Constants.ROWS;
+
+            if (IsPlayer1Turn())
+            {
+                Player2Bitboard &= ~moveBit;
+            }
+            else
+            {
+                Player1Bitboard &= ~moveBit;
+            }
+        }
+        
+        private int GetColumnHeight(int column)
         {
             return BitOperations.PopCount((Player1Bitboard | Player2Bitboard) & GetColumnMask(column));
         }
