51e4eae2d8ec3b934be93e12403c88bccdac94f8
Ian Donnelly <hello@ian-donnelly.dev>
2024-06-18 17:03:21 -0700

    feat: add function to stringify board state

--- Program.cs
+++ Program.cs
@@ -5,6 +5,44 @@ namespace ConnectFourEngine
 {
     static class Program
     {
+        private static string BoardToString(Board board)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int row = Constants.ROWS - 1; row >= 0; row--)
+            {
+                for (int col = 0; col < Constants.COLUMNS; col++)
+                {
+                    int bitIndex = col * Constants.ROWS + row;
+                    ulong mask = 1UL << bitIndex;
+                    
+                    if ((board.Player1Bitboard & mask) != 0)
+                    {
+                        sb.Append('x');
+                    }
+                    else if ((board.Player2Bitboard & mask) != 0)
+                    {
+                        sb.Append('o');
+                    }
+                    else
+                    {
+                        sb.Append('.');
+                    }
+
+                    if (col < Constants.COLUMNS - 1)
+                    {
+                        sb.Append(' ');
+                    }
+                }
+                if (row > 0)
+                {
+                    sb.AppendLine();
+                }
+            }
+
+            return sb.ToString();
+        }
+        
         private static string BitboardToString(ulong bitboard)
         {
             StringBuilder sb = new StringBuilder();
@@ -38,15 +76,19 @@ namespace ConnectFourEngine
         
         static void Main()
         {
-            const ulong bitboard = 0b000000_000000_000000_000000_000000_000000_000001;
+            Board board = new Board();
             
-            Console.WriteLine(BitboardToString(bitboard));
+            board.MakeMove(3);
+            board.MakeMove(3);
+            board.MakeMove(4);
+            
+            Console.WriteLine(BoardToString(board));
             Console.WriteLine();
             
-            Console.WriteLine(UlongToString(bitboard));
+            Console.WriteLine(BitboardToString(board.Player1Bitboard));
             Console.WriteLine();
             
-            Console.WriteLine(UlongToString(bitboard, false));
+            Console.WriteLine(BitboardToString(board.Player2Bitboard));
         }
     }
 }
