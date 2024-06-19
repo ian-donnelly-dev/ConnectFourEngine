8c387fc482fa1219a9b737d2dab45fd42a079f47
Ian Donnelly <hello@ian-donnelly.dev>
2024-06-19 00:47:38 -0700

    feat: add functions to export game state

--- Program.cs
+++ Program.cs
@@ -72,11 +72,60 @@ namespace ConnectFourEngine
             return Convert.ToString((long)bitboard, 2).PadLeft(length, '0');
         }
         
+        private static string ExportBitboardsString(Board board)
+        {
+            string player1BitboardString = ConvertToBinaryString(board.Player1Bitboard);
+            string player2BitboardString = ConvertToBinaryString(board.Player2Bitboard);
+
+            return $"({player1BitboardString}, {player2BitboardString})";
+
+            string ConvertToBinaryString(ulong bitboard)
+            {
+                StringBuilder sb = new StringBuilder("0b");
+                for (int col = Constants.COLS - 1; col >= 0; col--)
+                {
+                    for (int row = Constants.ROWS - 1; row >= 0; row--)
+                    {
+                        int bitIndex = col * Constants.ROWS + row;
+                        sb.Append((bitboard & (1UL << bitIndex)) != 0 ? '1' : '0');
+                    }
+                    if (col > 0)
+                    {
+                        sb.Append('_');
+                    }
+                }
+                return sb.ToString();
+            }
+        }
+
+        private static string ExportStateString(Board board)
+        {
+            StringBuilder stateString = new StringBuilder(Constants.BOARD_SIZE);
+            for (int i = 0; i < Constants.BOARD_SIZE; i++)
+            {
+                ulong mask = 1UL << i;
+                if ((board.Player1Bitboard & mask) != 0)
+                {
+                    stateString.Append('1');
+                }
+                else if ((board.Player2Bitboard & mask) != 0)
+                {
+                    stateString.Append('2');
+                }
+                else
+                {
+                    stateString.Append('0');
+                }
+            }
+            return stateString.ToString();
+        }
+        
         static void Main()
         {
             Board board = new Board();
 
             board.LoadBitboards(0b01, 0b10);
+            Console.WriteLine(ExportBitboardsString(board));
             Console.WriteLine(BoardToString(board));
             Console.WriteLine();
             
@@ -85,6 +134,7 @@ namespace ConnectFourEngine
             Console.WriteLine();
             
             board.LoadStateString("121200000000000000000000000000000000121212");
+            Console.WriteLine(ExportStateString(board));
             Console.WriteLine(BoardToString(board));
         }
     }
