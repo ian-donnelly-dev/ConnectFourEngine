7346b5d0b77f5ed80ab0f6c1c91e8a2886f6a848
Ian Donnelly <hello@ian-donnelly.dev>
2024-06-23 01:28:58 -0700

    feat: add Solvers class and implement Minimax with alpha-beta pruning

--- Program.cs
+++ Program.cs
@@ -8,10 +8,24 @@ namespace ConnectFourEngine
         {
             Board board = new Board();
             
-            board.ImportBoardState("121211_110000_000000_000000_000000_200000_220000");
+            board.ImportBoardState("000000_000000_100000_120000_000000_200000_000000");
             
             Console.WriteLine(board.StringifyBoard());
-            Console.WriteLine(board.CheckWin(true) || board.CheckWin(false));
+            Console.WriteLine();
+            
+            for (int col = 0; col < Constants.COLS; col++)
+            {
+                if (!board.IsColumnPlayable(col))
+                {
+                    continue;
+                }
+
+                board.MakeMove(col);
+                int score = Solvers.Minimax(board, board.IsPlayer1Turn(), Constants.MINIMAX_DEPTH_LIMIT, -Constants.MAX_SCORE, +Constants.MAX_SCORE);
+                board.UnmakeMove(col);
+
+                Console.WriteLine($"Column: {col}, Score: {score}");
+            }
         }
     }
 }
---
+++ Solvers.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConnectFourEngine
+{
+    public static class Solvers
+    {
+        public static int Minimax(Board board, bool maximizingPlayer, int depth, int alpha, int beta)
+        {
+            bool player1Won = !maximizingPlayer && board.IsWinner(true);
+            bool player2Won = maximizingPlayer && board.IsWinner(false);
+            bool tie = board.IsBoardFull();
+            
+            if (tie || player1Won || player2Won || depth == 0)
+            {
+                if (player1Won)
+                {
+                    return 22 - board.GetMovesCount(true);
+                }
+
+                if (player2Won)
+                {
+                    return board.GetMovesCount(false) - 22;
+                }
+    
+                return 0;
+            }
+            
+            int bestScore = maximizingPlayer ? -Constants.MAX_SCORE : +Constants.MAX_SCORE;
+            
+            for (int col = 0; col < Constants.COLS; col++)
+            {
+                if (!board.IsColumnPlayable(col))
+                {
+                    continue;
+                }
+            
+                board.MakeMove(col);
+                int score = Minimax(board, !maximizingPlayer, depth - 1, alpha, beta);
+                board.UnmakeMove(col);
+            
+                if (maximizingPlayer)
+                {
+                    bestScore = Math.Max(bestScore, score);
+                    alpha = Math.Max(alpha, bestScore);
+                }
+                else
+                {
+                    bestScore = Math.Min(bestScore, score);
+                    beta = Math.Min(beta, bestScore);
+                }
+            
+                if (beta <= alpha)
+                {
+                    break;
+                }
+            }
+
+            return bestScore;
+        }
+    }
+}
